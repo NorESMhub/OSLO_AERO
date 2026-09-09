2163023d9f955a6917d326a6642f38a385b6c646
sarambl <samebr@gmail.com>
2026-09-09 14:33:27 +0200

    Removed time-wasting call to hist_fld_active

--- src/oslo_aero_ndrop.F90
+++ src/oslo_aero_ndrop.F90
@@ -1474,10 +1474,7 @@ contains
    call ccncalc_oslo(state, pbuf, cs, hasAerosol, numberConcentration, volumeConcentration, &
       hygroscopicity, lnSigma, ccn)
    do isat = 1, psat
-       if (hist_fld_active(ccn_name(isat))) then
-          call outfld(ccn_name(isat), ccn(:ncol,:,isat), ncol, lchnk)
-
-       end if
+       call outfld(ccn_name(isat), ccn(:ncol,:,isat), ncol, lchnk)
    enddo
 
     tendencyCounted(:)=.FALSE.
